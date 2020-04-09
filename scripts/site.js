ce4b96653f39eac64416c72c66a39f8b5708c5ac
Tiago Dias <tigo@webdeleve.net>
2020-04-09 14:51:52 -0300

    Destructuring Syntax

--- scripts/site.js
+++ scripts/site.js
@@ -90,10 +90,12 @@ function loadApplication() {
 
     if (la != undefined) {
 
-        var isEmployed = la.Factors[0];
-        var hasKids = la.Factors[1];
-        var hasLoans = la.Factors[2];
-        var hasCreditcards = la.Factors[3];
+        var [
+            isEmployed,
+            hasKids,
+            hasLoans,
+            hasCreditcards,
+        ] = la.Factors;
 
 
         document.getElementById("inputName").value = la.ApplicantName;
