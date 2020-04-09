e1a6af556cfa61eaa00c23b3f4f1cd9ad7023155
Tiago Dias <tigo@webdeleve.net>
2020-04-09 14:22:24 -0300

    Create a Tagged Template Literal

--- scripts/site.js
+++ scripts/site.js
@@ -111,7 +111,7 @@ function loadApplication() {
 
         var riskLabel = document.getElementById("riskSummary");
         riskLabel.style.display = "block";
-        riskLabel.innerText = generateRickProfile(la); 
+        riskLabel.innerHTML = generateRickProfile(la); 
 
     }
 }
@@ -133,7 +133,7 @@ function saveApplication() {
 
         var riskLabel = document.getElementById("riskSummary");
         riskLabel.style.display = "block";
-        riskLabel.innerText = generateRickProfile(newLa);    
+        riskLabel.innerHTML = generateRickProfile(newLa);    
 
         LoanApplicationList.push(newLa);
 
@@ -339,9 +339,9 @@ function generateRickProfile(la) {
     var reviewText = "";
 
     if (age < 18) {
-        reviewText = "your application will not be reviewed, because you have to be 18 years or older.";
+        reviewText = "will not be reviewed, because you have to be 18 years or older";
     } else {
-        reviewText = "your application will be reviewed.";
+        reviewText = "will be reviewed";
     }
 
     var riskProfile = "";
@@ -356,10 +356,51 @@ function generateRickProfile(la) {
         riskProfile = "high";
     }
 
-    var summaryText = "Dear " + la.ApplicantName + ", " + reviewText + " Your risk profile is " + riskProfile;
+
+    var applicationCode = String.raw `\t${createApplicationId()}`;
+
+
+    var summaryText = highlighText `Dear ${la.ApplicantName}, <br>
+    your application for ${"$" + la.LoanAmount}, ${reviewText}. <br>
+    Your risk profile is ${riskProfile} <br>
+    Your risk profile is ${riskProfile} <br>
+    Your unique application code is ${applicationCode}`;
 
     return summaryText;
+
 }
 
 
+
+
+function highlighText(strings, ...values) {
+
+    let str = '';
+    for (let i = 0; i < strings.raw.length; i++) {
+        
+        if (i>0) {
+            str += `<b>${values[i-1]}</b>`;            
+        }
+
+        str += strings.raw[i];
+        
+    }
+
+    return str;
+
+}
+
+
+function createApplicationId() {
+
+    var result = '';
+    var characters =  'ASBHJFsdifjsom02892/\\#@$&%*'
+    var charactersLength = characters.length;
+    for (var i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+
+    return result;
+
+}
 
